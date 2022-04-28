5c0cf19967ab883f01aec4f47726c05d7f80c5e6
wangyongxin0408 <wangyongxin0408@shihuo.cn>
2022-04-28 17:57:03 +0800

    Initial commit

---
+++ Example/ZFPlayer/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  ZFPlayer
+//
+//  Created by wangyongxin0408 on 04/28/2022.
+//  Copyright (c) 2022 wangyongxin0408. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZFPlayer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZFPlayer
+//
+//  Created by wangyongxin0408 on 04/28/2022.
+//  Copyright (c) 2022 wangyongxin0408. All rights reserved.
+//
+
+@import UIKit;
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+    }
+}
